2b5b55b329be99216e0a413dcf8a9fff4115cc39
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-04-02 18:20:31 +0100

    Update TripAdapter.java

--- app/src/main/java/com/example/m_expense/TripAdapter.java
+++ app/src/main/java/com/example/m_expense/TripAdapter.java
@@ -9,7 +9,6 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
